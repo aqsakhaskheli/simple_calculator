cf8a2b9c95054b305c4e4de746267c9e9d1e35a9
aqsakhaskheli <aqsakhaskheli09@gmail.com>
2024-03-19 01:36:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+// Asking Qustions for Users through inquirer
+let answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "subtraction", "Multiplication", "Division"],
+    },
+]);
+// conditional statment If-Else
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
